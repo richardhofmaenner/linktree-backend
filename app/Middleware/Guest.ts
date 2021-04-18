6ab328027157aeaeb7d415fcf10f7ff48733f4bd
Richard Hofmänner <hello@terillos.dev>
2021-04-18 20:44:52 +0200

    Fixed eslint error

--- app/Middleware/Guest.ts
+++ app/Middleware/Guest.ts
@@ -1,6 +1,7 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class Guest {
+  // eslint-disable-next-line consistent-return
   public async handle({ auth, response }: HttpContextContract, next: () => Promise<void>) {
     if (auth.isAuthenticated) {
       return response.status(401)
